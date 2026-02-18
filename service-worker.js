633a3cbfb2021b0afb59cf92e66aab3d8f3e2bc0
eduardo2145-stack <eduardo2145@gmail.com>
2026-02-18 05:39:17 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "amigo-virtual-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./mobile.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
